5dd7d6c74fcbfbdaa489dbb1ce55ff34b411876c
p-chen <31415926535897pearl@gmail.com>
2016-10-13 06:12:36 -0400

    hi

--- git thing/ViewController.swift
+++ git thing/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let thing = "apples are better than pineapples"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
